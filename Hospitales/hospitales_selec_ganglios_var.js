15abf05fb0efa05270bf904fe7402b316c01d55f
smaule1 <samuelzv18@gmail.com>
2025-11-17 12:30:12 -0600

    marcadores de hospitales y clinicas

---
+++ Hospitales/hospitales_selec_ganglios_var.js
@@ -0,0 +1,8 @@
+var hospitalesGanglios = {
+"type": "FeatureCollection",
+"name": "hospitales_selec_ganglios",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "HOSPITAL DR. TOMAS CASAS CASAJUS CIUDAD CORTES", "TELEFONO": "2788-8197 / 2788-8148 / 2788-8198", "PROVINCIA": "PUNTARENAS", "CANTON": "OSA", "DISTRITO": "PUERTO CORTES" }, "geometry": { "type": "Point", "coordinates": [ -83.543773491453095, 8.987427184183092 ] } }
+]
+}
